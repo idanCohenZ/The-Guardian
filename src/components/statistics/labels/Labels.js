f17b77d2bd59d6adeeb754050a49f32db254a11b
RoiYehezkel <roi5634@gmail.com>
2023-07-24 15:21:01 +0300

    change labels page design

--- src/components/statistics/labels/Labels.js
+++ src/components/statistics/labels/Labels.js
@@ -1,40 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 import "./Labels.css";
 
 function Labels({ labels }) {
+  const count = labels.reduce((accumulator, label) => {
+    return accumulator + label.labelCount;
+  }, 0);
+
   return (
     <>
       <img
-        className="backgroung-labels-image"
         src="/images/background-labels.png"
-        alt="backgroung lables page"
+        alt="backgroung-category"
+        className="backgroung-cat-image"
       />
-
-      <h1 className="label-header">Labels</h1>
-      <div className="labels-cards">
-        <div className="labels-container">
-          <div className="row labels-wrapper">
-            {labels.map((labelobj, index) => (
-              <Card label={labelobj} key={index} />
-            ))}
-          </div>
+      <h1 className="cat-header">Categories</h1>
+      <div className="cat-container">
+        <div className="cat-table">
+          {labels.map((labelobj, index) => (
+            <CircleProgress
+              category={labelobj.str}
+              labelCount={labelobj.labelCount}
+              count={count}
+              key={index}
+            />
+          ))}
         </div>
       </div>
     </>
   );
 }
 
-function Card({ label }) {
+function CircleProgress({ category, labelCount, count }) {
+  const [val, setVal] = useState(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const relativeSum = 100 / count;
+      const percentage = labelCount * relativeSum;
+      setVal(percentage.toFixed(2));
+    }, 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [labelCount, count]);
+
   return (
-    <div className="col-lg-2">
-      <div className="our-team-main">
-        <div className="team-front">
-          <h3>{label.string}</h3>
-        </div>
-        <div className="team-back">
-          <span>Frequency: {label.frequency}</span>
-        </div>
+    <div className="circle-container">
+      <div className="circle-wrapper">
+        <CircularProgressbar value={val} text={`${val}%`} />
       </div>
+      <p className="circle-text">{category}</p>
     </div>
   );
 }
